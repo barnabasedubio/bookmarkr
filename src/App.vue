904ed5ca1eb114861f08c8361ebb4a90fbfb459c
Barnabas Edubio <barny.edubio@gmail.com>
2020-11-29 23:18:59 +0100

    Added component boilerplate, mild changes to design file

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<the-header></the-header>
+    <h1>Hello World.</h1>
+    <base-button></base-button>
+	<the-footer></the-footer>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TheHeader from "./components/TheHeader.vue"
+import TheFooter from "./components/TheFooter.vue"
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "App",
+    components: {
+		TheHeader,
+		TheFooter
+	}
+};
 </script>
 
 <style>
+* {
+	margin: 0;
+	padding: 0;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 }
 </style>
